9d588ab801361d1563cad3b644e50f95f117ac0c
João Lucas <jlucasmartins2907@gmail.com>
2025-08-31 03:54:52 -0300

    Frontend concluído.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Trabalho Sitemas Distribuídos
+
+## Integrantes: 
+ - João Lucas Raposo Martins
+ - Pablo Werner Miranda
+
+## Tencnologias Usadas:
+* **Frontend:** React, Tailwind CSS
---
+++ frontend-login-request/src/App.jsx
@@ -0,0 +1,178 @@
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+
+function App() {
+  const [emailInputConfig, setEmailInputConfig] = useState({
+    isSelected: false,
+    containsText: false,
+  });
+  const [passwordInputConfig, setPasswordInputConfig] = useState({
+    isSelected: false,
+    containsText: false,
+    showPassword: false,
+  });
+  const [authorization, setAuthorization] = useState(null);
+
+  const port = 3000;
+
+  async function validateUser(form, email, password) {
+    form.preventDefault();
+
+    setAuthorization("loading");
+
+    try {
+      const response = await fetch(`http://localhost:${port}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+
+      if (response.status !== 200) {
+        console.log("Erro na requisição: " + response.status);
+        return; // Retorne para não processar a resposta
+      }
+
+      const data = await response.json();
+
+      if (data.authorization === "authorized") {
+        setAuthorization("authorized");
+      }
+    } catch (e) {
+      setAuthorization(null);
+      console.log(e);
+    }
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center m-15">
+      <form
+        onSubmit={(form) =>
+          validateUser(
+            form,
+            emailInputConfig.containsText,
+            passwordInputConfig.containsText
+          )
+        }
+        action=""
+        className="flex flex-col justify-center w-[500px] p-10 bg-white border-slate-600 border-[1px] shadow-sm shadow-black rounded-2xl gap-10"
+      >
+        <h1 className="text-center font-bold text-[16pt]">Entrar na conta</h1>
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col items-start relative">
+            <label
+              htmlFor="userEmail"
+              className={`absolute translate-x-[10px] -translate-y-1/2 cursor-pointer transition duration-200 font-bold bg-white pr-2 pl-2 ${
+                emailInputConfig.isSelected || emailInputConfig.containsText
+                  ? "text-slate-500"
+                  : "  text-slate-300 translate-y-[65%]"
+              } `}
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="userEmail"
+              className={`border-[2px] border-slate-300 rounded text-[14pt] p-3 focus:border-slate-500 transition duration-200 cursor-pointer w-full ${
+                emailInputConfig.containsText && "border-slate-500"
+              }`}
+              onFocus={() =>
+                setEmailInputConfig((prev) => ({ ...prev, isSelected: true }))
+              }
+              onBlur={() =>
+                setEmailInputConfig((prev) => ({ ...prev, isSelected: false }))
+              }
+              onChange={(e) =>
+                setEmailInputConfig((prev) => ({
+                  ...prev,
+                  containsText: e.target.value,
+                }))
+              }
+            />
+          </div>
+          <div className="flex flex-col relative">
+            <label
+              htmlFor="userPassword"
+              className={`absolute translate-x-[10px] -translate-y-1/2 cursor-pointer transition duration-200 font-bold bg-white pr-2 pl-2 ${
+                passwordInputConfig.isSelected ||
+                passwordInputConfig.containsText
+                  ? "text-slate-500"
+                  : "  text-slate-300 translate-y-[65%]"
+              } `}
+            >
+              Senha
+            </label>
+            <input
+              type={passwordInputConfig.showPassword ? "text" : "password"}
+              id="userPassword"
+              className={`border-[2px] border-slate-300 rounded text-[14pt] p-3 focus:border-slate-500 transition duration-200 cursor-pointer w-full ${
+                passwordInputConfig.containsText && "border-slate-500"
+              }`}
+              onFocus={() =>
+                setPasswordInputConfig((prev) => ({
+                  ...prev,
+                  isSelected: true,
+                }))
+              }
+              onBlur={() =>
+                setPasswordInputConfig((prev) => ({
+                  ...prev,
+                  isSelected: false,
+                }))
+              }
+              onChange={(e) =>
+                setPasswordInputConfig((prev) => ({
+                  ...prev,
+                  containsText: e.target.value,
+                }))
+              }
+            />
+            <span
+              onClick={() =>
+                setPasswordInputConfig((prev) => ({
+                  ...prev,
+                  showPassword: !passwordInputConfig.showPassword,
+                }))
+              }
+              className="absolute right-5 top-1/2 -translate-y-1/2 cursor-pointer"
+            >
+              {passwordInputConfig.showPassword ? (
+                <EyeOff
+                  className={`${
+                    passwordInputConfig.containsText
+                      ? "text-slate-500"
+                      : "text-slate-300"
+                  } hover:text-slate-500 transition duration-200`}
+                />
+              ) : (
+                <Eye
+                  className={`${
+                    passwordInputConfig.containsText
+                      ? "text-slate-500"
+                      : "text-slate-300"
+                  } hover:text-slate-500 transition duration-200`}
+                />
+              )}
+            </span>
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className={`cursor-pointer border-[2px] border-blue-300 p-3 rounded-lg font-bold hover:bg-blue-300 hover:text-white text-blue-300 transition duration-200`}
+          disabled={authorization == "loading" ? true : false}
+        >
+          {authorization == "loading" ? "Entrando..." : "Entrar"}
+        </button>
+
+        <h2 className="text-">Feito por: João Lucas & Pablo</h2>
+      </form>
+    </div>
+  );
+}
+
+export default App;
